ce216af1beb5cb3a122e99362ee1ae79438f9823
Seojin Kim <1106laura@naver.com>
2020-03-20 00:26:44 +0900

    2020-03-20 : utf-8 error is so hard..

--- CodeScraper/urls.py
+++ CodeScraper/urls.py
@@ -4,5 +4,5 @@ from . import views
 urlpatterns=[
   path('', views.index, name='index'),
   path('process',views.process, name='process'),
-  path('result/<lang>/<q_id>', views.result, name='result')
+  path('result/<lang>/<q_id>/', views.result, name='result')
 ]

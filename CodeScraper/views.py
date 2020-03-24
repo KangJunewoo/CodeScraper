903e058f71fc0499244f3546f0745b3adc4e9665
Seojin Kim <1106laura@naver.com>
2020-03-25 02:18:05 +0900

    2020-03-25 : indent 제외한 에러 해결!

--- CodeScraper/views.py
+++ CodeScraper/views.py
@@ -8,7 +8,9 @@ from requests import get
 code_list = []
 
 def index(request):
-  code_list = []
+  code_list.clear()
+  print("list cleared")
+  print(code_list)
   return render(request, 'index.html')
 
 def process(request):
@@ -16,61 +18,58 @@ def process(request):
   q_id=request.POST['oj_url'].split('/')[-1]
   lang=request.POST['lang']
   searching = f"백준 {q_id} {lang}"
+  
+  i=0
 
-  values={
-    'q':searching,
-    'oq':searching,
-    'aqs':'chrome..69i57.35694j0j7',
-    'sourceid':'chrome',
-    'ie':'UTF-8',
-  }
+  while (len(code_list) <= 4) and (i<20) :
+    values={
+      'q':searching,
+      'oq':searching,
+      'aqs':'chrome..69i57.35694j0j7',
+      'sourceid':'chrome',
+      'ie':'UTF-8',
+      'start': i
+    }
+    hdr={'User-Agent':'Mozilla/5.0'}
 
-  hdr={'User-Agent':'Mozilla/5.0'}
+    querystring=urllib.parse.urlencode(values)
+    req=urllib.request.Request(base_url+'?'+querystring, headers=hdr)
+    context=ssl._create_unverified_context()
+    try:
+      res=urllib.request.urlopen(req, context=context)
+    except:
+      traceback.print_exc()
+      break
 
-  querystring=urllib.parse.urlencode(values)
-  req=urllib.request.Request(base_url+'?'+querystring, headers=hdr)
-  context=ssl._create_unverified_context()
-  try:
-    res=urllib.request.urlopen(req, context=context)
-  except:
-    traceback.print_exc()
-
-  soup=BeautifulSoup(res.read(), 'html.parser')
+    i += 10
+    soup=BeautifulSoup(res.read(), 'html.parser')
   #print(soup.prettify('utf-8'))
-  targets = soup.find_all('a')[17:27]
-  target_url_list=[]
-  for target in targets:
-    target_url_list.append('https://www.google.com'+target.get("href"))
-  #print(target_url_list)
-  #return redirect('result', lang=lang, q_id=q_id)
-  codes_list = []
-  for url in target_url_list:
-    result = requests.get(url, headers={ 'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; WOW64; Trident/7.0; rv:11.0) like Gecko'})
-    result.encoding = "utf-8"
-    soup = BeautifulSoup(result.content.decode("utf-8"), "html.parser")
-    print("Scraping site url : "+url)
-    if soup.find("div", {"class":"colorscripter-code"}):
-      print("got colorscripter")
-      codes_list.append(soup.find_all("div", {"class":"colorscripter-code"}))
-    elif soup.find("div", {"class":"se_code"}):
-      print("got __se_code_view")
-      codes_list.append(soup.find_all("div", {"class":"se_code"}))
-    elif soup.find("table", {"class":"highlight"}):
-      print("got highlightjs")
-      codes_list.append(soup.find_all("table", {"class":"highlight"}))
-#왠진 모르겠지만 se_code랑 highlightjs가 포함된 html들은 제대로 긁혀 오지가 않네요..
-    elif soup.find("code"):
-      print("got code")
-      codes_list.append(soup.find_all("code"))
-    else:
-      continue
-  for codes in codes_list:
-    #여기를 lstrip하면 왼쪽 공백이 사라질 줄 알았는데ㅜㅜ
-    code_list.append(str(codes)[1:-1].lstrip())
-    print(type(str(codes)))
+    targets = soup.find_all('a')[17:27]
+
+    target_url_list=[]
+    for target in targets:
+      target_url_list.append('https://www.google.com'+target.get("href"))
+    #print(target_url_list)
+    #return redirect('result', lang=lang, q_id=q_id)
+    
+    for url in target_url_list:
+      result = requests.get(url, headers={ 'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; WOW64; Trident/7.0; rv:11.0) like Gecko'})
+      result.encoding = "utf-8"
+      soup = BeautifulSoup(result.content.decode("utf-8"), "html.parser")
+      print("Scraping site url : "+url)
+      if soup.find("div", {"class":"colorscripter-code"}):
+        print("♪ got colorscripter")
+        for code in soup.find_all("div", {"class":"colorscripter-code"}):
+          code_list.append(str(code))
+      elif soup.find("code"):
+        print("♪ got code")
+        for code in soup.find_all("code"):
+          code_list.append(str(code).strip())
+      else:
+        continue
+  #code_list = list(set(code_list))
   return redirect('result', lang=lang, q_id=q_id)
   #return redirect(f'result/{lang}/{q_id}')
 
 def result(request, lang, q_id):
-  #print(code_list)
   return render(request, 'result.html', {'q_id':q_id, 'lang':lang, 'code_list':code_list})
